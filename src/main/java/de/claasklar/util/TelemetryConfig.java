7331639a81dac7f8ba4fcabee3fb327e571d38d6
claasklar <git@claasklar.de>
2023-07-02 19:08:07 +0200

    fix: use elasticsearch as storage backend
    the badger storage backend needs too much memory

--- src/main/java/de/claasklar/util/TelemetryConfig.java
+++ src/main/java/de/claasklar/util/TelemetryConfig.java
@@ -50,7 +50,7 @@ public class TelemetryConfig {
                         OtlpGrpcSpanExporter.builder().setEndpoint("http://jaeger:4317").build())
                     .build())
             .setResource(resource)
-            .setSampler(new CustomAlwaysSampler(parentBased(traceIdRatioBased(0.02))))
+            .setSampler(new CustomAlwaysSampler(parentBased(traceIdRatioBased(0.005))))
             .build();
 
     SdkMeterProvider sdkMeterProvider =
